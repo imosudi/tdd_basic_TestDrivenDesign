78e7af4711d8d1bb012c41aa1a49962899e5381e
imosudi <imosudi@gmail.com>
2017-08-12 16:55:16 +0100

    Sat Aug 12 16:55:16 WAT 2017

--- app/league_performance.py
+++ app/league_performance.py
@@ -1,13 +1,7 @@
 class LeastGoalPerformer(object):
 	filename = 'football-league-result.txt'
 
-	'''def confirmFileType(self, filename):
-					if filename.endswith('.txt')
-					pass'''
-	def confirmFileIntergrity():
-		if os.path.isfile(filename):
-			return filename
-		pass
+	
 
 
 
