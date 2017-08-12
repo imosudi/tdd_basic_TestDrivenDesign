32925613ac34bd5afa51929319431369188d4ec7
imosudi <imosudi@gmail.com>
2017-08-12 15:46:48 +0100

    Sat Aug 12 15:46:48 WAT 2017

---
+++ test/test_league_performance.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TddLeastGoalPerformer(unittest.Testcase):
+	
+
+
+
+
+
+
+	"""docstring for TddLeastGoalPerformer"""
+	def __init__(self, arg):
+		super(TddLeastGoalPerformer, self).__init__()
+		self.arg = arg
