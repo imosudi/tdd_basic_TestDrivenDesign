ae5a0bcf9041e3e345f05f4ae46343fb9c872af2
imosudi <imosudi@gmail.com>
2017-08-12 11:16:50 +0100

    Sat Aug 12 11:16:50 WAT 2017

---
+++ ph_weather.py
@@ -0,0 +1,31 @@
+filename = 'port-harcourt-weather.txt'
+def weatherList(filename):
+	with open(filename) as phweather:
+		dailyList = []
+		dailyHighList = []
+		dailyLowList = []
+		dailyTempSpread = []
+		phweather.next() #Mosudi Skipping the first line
+		phweather.next() #Mosudi Skipping the second line#I will later use confirm if list empty
+		for line in phweather: #interating the lines
+			line.strip()
+			dayDailyTempSpread = []
+			#print line
+			line = line.split() #creating list for each line
+			#print line
+			try: #attempt to extract my relevant data, namely cols: 1,2 & 3
+				dayNum = int(line[0]) #avoid the last row starting with string "mo", even if the month is 31
+				dailyHigh = int(line[1]) 
+				dailyLow =  int(line[2])
+			except ValueError:
+				pass
+			#print dayNum, dailyHigh, dailyLow, (dailyHigh - dailyLow)
+			dailyList.append(dayNum), dailyHighList.append(dailyHigh), dailyLowList.append(dailyLow), dailyTempSpread.append(dailyHigh - dailyLow)
+
+	weatherDict = dict(zip(dailyList, dailyTempSpread))
+	#print weatherDict
+	#print min(sorted(weatherDict.values()))
+	print "The day with the smallest temperature spread is  day: " + str(min(weatherDict, key=weatherDict.get)) + ", with temperature spread of " +  str(min(sorted(weatherDict.values())))
+
+
+weatherList(filename)
